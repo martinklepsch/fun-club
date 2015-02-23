efc3d077521b23ab966f033df99d5cba775461da
Martin Klepsch <martinklepsch@googlemail.com>
2015-02-23 13:52:19 +0100

    more complex updating

--- src/cljs/fcc/app.cljs
+++ src/cljs/fcc/app.cljs
@@ -1,6 +1,10 @@
 (ns fcc.app
   (:require [rum]))
 
+(def letters ["alpha" "beta" "gamma" "delta" "epsilon" "zeta" "eta"
+              "theta" "iota" "kappa" "lambda" "mu" "nu" "xi" "omicron"
+              "pi" "rho" "sigma" "tau" "upsilon" "phi" "chi" "psi" "omega"])
+
 (defonce app-state (atom {:heading "The Greek Alphabet"
                           :letters ["alpha" "beta" "gamma"]}))
 
@@ -14,5 +18,11 @@
 (defn update-state []
   (swap! app-state update-in [:letters] conj "delta"))
 
+(defn update-state []
+  (let [letter-count (count (:letters @app-state))]
+    (if (< letter-count (count letters))
+      (swap! app-state update-in [:letters] conj (nth letters letter-count)))))
+
 (defn init []
-  (rum/mount (app app-state) (.-body js/document)))
+  (rum/mount (app app-state) (.-body js/document))
+  (.setInterval js/window update-state 300))
